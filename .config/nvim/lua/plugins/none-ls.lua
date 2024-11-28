ee08d873d5b89d4185b4e3d828f08882c43c77fe
YumaNagayoshi <s.memo.yuma@gmail.com>
2024-11-29 02:13:02 +0900

    rubocop起動時に新しいルールが未記入で警告出るので無視するように調整

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -5,7 +5,13 @@ null_ls.setup({
     null_ls.builtins.formatting.prettier,
     require("none-ls.diagnostics.eslint"),
     require("none-ls.code_actions.eslint"),
-    null_ls.builtins.diagnostics.rubocop,
-    null_ls.builtins.formatting.rubocop
+    null_ls.builtins.diagnostics.rubocop.with({
+      command = "rubocop",
+      args = {"--disable-plugins"},
+    }),
+    null_ls.builtins.formatting.rubocop.with({
+      command = "rubocop",
+      args = {"--disable-plugins"},
+    })
   }
 })
