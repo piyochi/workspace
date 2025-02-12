d3d6dafcd764ed7ff27c9e42ca4a2780d373a46c
YumaNagayoshi <s.memo.yuma@gmail.com>
2025-02-13 08:24:53 +0900

    github copilotをデフォルトで有効に

--- .config/nvim/lua/plugins/github-copilot.lua
+++ .config/nvim/lua/plugins/github-copilot.lua
@@ -1,13 +1,15 @@
--- nvimが起動したらcopilotを無効にする
+-- nvimが起動したらcopilotを有効にする
 vim.api.nvim_create_autocmd("VimEnter", {
-  pattern = "*",
+  -- pattern = "*",
   callback = function()
-    vim.api.nvim_command("Copilot disable")
+    -- vim.api.nvim_command("Copilot disable")
+    vim.cmd("Copilot enable")
   end,
 })
 
 -- \cpでCopilotの有効無効を切り替える
-local copilot_enabled = false
+-- local copilot_enabled = false
+local copilot_enabled = true
 function ToggleCopilot()
   if copilot_enabled then
     vim.cmd("Copilot disable")
